660357cdd712747f5ce06e06c56da3d25e2ba656
Josh McKinney <joshka@users.noreply.github.com>
2022-12-05 14:11:29 -0800

    Remove browser action

--- src/background-script.js
+++ src/background-script.js
@@ -1,25 +1,3 @@
-function onError(error) {
-    console.error(`Failed to execute follow content script: ${error}`);
-}
-
-browser.browserAction.onClicked.addListener(() => {
-    browser.tabs
-        .executeScript({file: "follow-user.js"})
-        .catch(onError);
-});
-
-browser.runtime.onInstalled.addListener(() => {
-    browser.storage.sync
-        .get('serverUrl')
-        .then((results) => {
-            if (!results || !results.serverUrl) {
-                browser.storage.sync.set({
-                    serverUrl: 'https://mastodon.social'
-                });
-            }
-        });
-}); 
-
 browser.runtime.onInstalled.addListener(async ({ reason, temporary }) => {
     // if (temporary) return; // skip during development
     switch (reason) {
@@ -29,4 +7,4 @@ browser.runtime.onInstalled.addListener(async ({ reason, temporary }) => {
         }
         break;
     }
-  });
+});
